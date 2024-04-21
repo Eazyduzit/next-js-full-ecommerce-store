7df04ca3e8c088b1a4f76343ee3fb921fd7efba2
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-21 17:13:30 +0200

    created get product data function and added data to Product card

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -27,8 +27,20 @@ async function getUserData() {
   }
 }
 
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ])
+  return { activeCount, inactiveCount }
+}
+
 export default async function AdminDashboard() {
-  const [salesData, userData] = await Promise.all([getSalesData(), getUserData()])
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ])
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -44,8 +56,8 @@ export default async function AdminDashboard() {
       />
       <DashboardCard
         title="Active Products"
-        subtitle={`${formatNumber()} Inactive Products`}
-        body={formatNumber()}
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   )
