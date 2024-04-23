cb174a63c66d73be32a97704a2d58b5a7ec5ea7f
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-23 12:05:33 +0200

    refactored ProductsTable to async to await is available or not, adding table cell with conditional for icons

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,6 +1,15 @@
 import { PageHeader } from "../_components/PageHeader"
 import { Button } from "@/components/ui/button"
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import db from "@/db/db"
+import { CheckCircle2, XCircle } from "lucide-react"
 import Link from "next/link"
 
 export default function AdminProductsPage() {
@@ -17,7 +26,20 @@ export default function AdminProductsPage() {
   )
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: "asc" },
+  })
+
+  if (products.length === 0) return <p>No products found</p>
+
   return (
     <Table>
       <TableHeader>
@@ -33,6 +55,25 @@ function ProductsTable() {
           </TableHead>
         </TableRow>
       </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <span className="sr-only">Available</span>
+                  <CheckCircle2 />
+                </>
+              ) : (
+                <>
+                  <span className="sr-only">Unavailable</span>
+                  <XCircle />
+                </>
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   )
 }
