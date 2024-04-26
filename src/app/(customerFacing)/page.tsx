b48ef8bf32ac8bff891aab1e6747dacb1b979fe7
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-26 12:02:49 +0200

    created product grid section and props, use component in HomePAge

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,5 @@
 import db from "@/db/db"
+import { Product } from "@prisma/client"
 
 function getMostPopularProducts() {
   return db.product.findMany({
@@ -17,5 +18,25 @@ function getNewestProducts() {
 }
 
 export default function HomePage() {
-  return <h1>Hi</h1>
+  return (
+    <main className="space-y-12">
+      <ProductGridSection title="Most Popular" productsFetcher={getMostPopularProducts} />
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+    </main>
+  )
+}
+
+type ProductGridSectionProps = {
+  title: string
+  productsFetcher: () => Promise<Product[]>
+}
+
+function ProductGridSection({ productsFetcher, title }: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+      </div>
+    </div>
+  )
 }
