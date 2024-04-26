f39d330a53b8129134086d6c34ded7f704418b39
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-26 13:10:10 +0200

    created Skeleton effect for Product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,20 +5,14 @@ import Link from "next/link"
 import { formatCurrency } from "@/lib/formatters"
 
 type ProductCardProps = {
-  id: number
+  id: string
   name: string
   priceInCents: number
   description: string
   imagePath: string
 }
 
-export default function ProductCard({
-  id,
-  name,
-  priceInCents,
-  description,
-  imagePath,
-}: ProductCardProps) {
+export function ProductCard({ id, name, priceInCents, description, imagePath }: ProductCardProps) {
   return (
     <Card className="flex overflow-hidden flex-col">
       <div className="relative w-full h-auto aspect-video">
@@ -35,6 +29,30 @@ export default function ProductCard({
         <Button asChild size="lg" className="w-full">
           <Link href={`/products${id}/purchase`}>Purchase</Link>
         </Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export function ProductCardSkeleton() {
+  return (
+    <Card className="flex overflow-hidden flex-col animate-pulse">
+      <div className="w-full aspect-video bg-gray-300" />
+      <CardHeader>
+        <CardTitle>
+          <div className="w-3/4 h-6 rounded-full bg-gray-300" />
+        </CardTitle>
+        <CardDescription>
+          <div className="w-1/2 h-4 rounded-full bg-gray-300" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="w-full h-4 rounded-full bg-gray-300" />
+        <div className="w-full h-4 rounded-full bg-gray-300" />
+        <div className="w-3/4 h-4 rounded-full bg-gray-300" />
+      </CardContent>
+      <CardFooter>
+        <Button disabled size="lg" className="w-full"></Button>
       </CardFooter>
     </Card>
   )
