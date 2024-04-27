10dcd9c0a95a930031eaa95ab7275ceb574f01a6
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-27 13:15:37 +0200

    created account on strip.com, added test keys for public and secret, added .env to gitignore so you cant see the strings here (sowwy not sowwy)

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,8 +1,27 @@
+"use client"
+
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+
 type CheckoutFormProps = {
   product: {}
   clientSecret: string
 }
 
+const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
+
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
-  return <h1>Form</h1>
+  return (
+    <Elements options={{ clientSecret }} stripe={stripe}>
+      <Form />
+    </Elements>
+  )
 }
+
+function Form() {
+  const stripe = useStripe()
+  const elements = useElements()
+  return <PaymentElement />
+}
+
+// Str1peT3st2024
