b609c447d47b5b1ccba196aecae27f0e1ffef33a
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-27 12:37:30 +0200

    setting up strip payment keys etc, NOT WORKING BC YOU NEED TO REGISTER TO STRIPE TO GENERATE KEYS

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,8 @@
 import db from "@/db/db"
 import { notFound } from "next/navigation"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({ params: { id } }: { params: { id: string } }) {
   const product = await db.product.findUnique({
@@ -7,5 +10,15 @@ export default async function PurchasePage({ params: { id } }: { params: { id: s
   })
   if (product == null) return notFound()
 
-  return <h1>Hi</h1>
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    metadata: { productId: product.id },
+  })
+
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent")
+  }
+
+  return <CheckoutForm product={product} client_secret={paymentIntent.client_secret} />
 }
