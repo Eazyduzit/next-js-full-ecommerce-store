e55e8615a4627482aebb9dedc3ddf54abb18b20a
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-27 14:52:48 +0200

    built checkout form

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,10 +1,17 @@
 "use client"
 
+import { formatCurrency } from "@/lib/formatters"
 import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
+import Image from "next/image"
 
 type CheckoutFormProps = {
-  product: {}
+  product: {
+    imagePath: string
+    name: string
+    priceInCents: number
+    description: string
+  }
   clientSecret: string
 }
 
@@ -12,16 +19,25 @@ const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
 
 export function CheckoutForm({ product, clientSecret }: CheckoutFormProps) {
   return (
-    <Elements options={{ clientSecret }} stripe={stripe}>
-      <Form />
-    </Elements>
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image src={product.imagePath} fill alt={product.name} className="object-cover" />
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">{product.description}</div>
+          <div className="text-lg">{formatCurrency(product.priceInCents / 100)}</div>
+        </div>
+      </div>
+      <Elements options={{ clientSecret }} stripe={stripe}>
+        <Form />
+      </Elements>
+    </div>
   )
 }
 
 function Form() {
-  const stripe = useStripe()
   const elements = useElements()
   return <PaymentElement />
 }
-
-// Str1peT3st2024
