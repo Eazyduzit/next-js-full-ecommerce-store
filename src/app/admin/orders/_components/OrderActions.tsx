8256111d067e29e8c1c439099cc7188b6ed5bd7d
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-30 12:22:21 +0200

    added admin order actions page

--- src/app/admin/orders/_components/OrderActions.tsx
+++ src/app/admin/orders/_components/OrderActions.tsx
@@ -1,81 +1,26 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import db from "@/db/db"
-import { formatCurrency } from "@/lib/formatters"
-import { PageHeader } from "../_components/PageHeader"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { MoreVertical } from "lucide-react"
-import { DeleteDropDownItem } from "./_components/OrderActions"
+"use client"
 
-function getOrders() {
-  return db.order.findMany({
-    select: {
-      id: true,
-      pricePaidInCents: true,
-      product: { select: { name: true } },
-      user: { select: { email: true } },
-    },
-    orderBy: { createdAt: "desc" },
-  })
-}
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { useTransition } from "react"
+import { deleteOrder } from "../../_actions/orders"
+import { useRouter } from "next/navigation"
 
-export default function OrdersPage() {
-  return (
-    <>
-      <PageHeader>Sales</PageHeader>
-      <OrdersTable />
-    </>
-  )
-}
-
-async function OrdersTable() {
-  const orders = await getOrders()
-
-  if (orders.length === 0) return <p>No sales found</p>
+export function DeleteDropDownItem({ id }: { id: string }) {
+  const [isPending, startTransition] = useTransition()
+  const router = useRouter()
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Product</TableHead>
-          <TableHead>Customer</TableHead>
-          <TableHead>Price Paid</TableHead>
-          <TableHead className="w-0">
-            <span className="sr-only">Actions</span>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {orders.map((order) => (
-          <TableRow key={order.id}>
-            <TableCell>{order.product.name}</TableCell>
-            <TableCell>{order.user.email}</TableCell>
-            <TableCell>{formatCurrency(order.pricePaidInCents / 100)}</TableCell>
-            <TableCell className="text-center">
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <MoreVertical />
-                  <span className="sr-only">Actions</span>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DeleteDropDownItem id={order.id} />
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <DropdownMenuItem
+      variant="destructive"
+      disabled={isPending}
+      onClick={() =>
+        startTransition(async () => {
+          await deleteOrder(id)
+          router.refresh()
+        })
+      }
+    >
+      Delete
+    </DropdownMenuItem>
   )
 }
